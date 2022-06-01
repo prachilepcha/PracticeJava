9aa49e1403a805366390f1c8e08ca4f0bb544243
Prachi Lepcha <prachi06lepcha@gmail.com>
2022-06-01 23:53:01 +0530

    Add files via upload

---
+++ SumOfNumbers.java
@@ -0,0 +1,14 @@
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		System.out.print("The sum of numbers is: ");
+		
+		int num1=10, num2=18, num3=2, sum;
+		sum = num1 + num2 + num3;
+		
+		System.out.print(sum);
+
+	}
+
+}
