3e11f62d30fe0ffd3e7b0b70ea40f662f08226d6
prachilepcha <prachi06lepcha@gmail.com>
2022-06-01 23:43:45 +0530

    Getter Setter Main

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.gettingValues;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Candidate cd = new Candidate();
+		cd.setId(12);
+		cd.setName("Hizashi");
+	}
+
+}
